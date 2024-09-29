6df3af866f39fa57c79c940116253fa230df7031
Sharan112233 <138100505+Sharan112233@users.noreply.github.com>
2024-09-29 11:23:39 +0530

    Update time.js

--- time.js
+++ time.js
@@ -3,7 +3,7 @@ var b1 = document.getElementById('b1')
 b1.addEventListener('click', function t1() {
     var time = new Date()
     var box = document.getElementById('box').innerHTML = `${time}`;
-    setInterval(t1,1000);
+   
 });
 
 
@@ -13,7 +13,7 @@ b2.addEventListener('click', function t2() {
     var time = new Date()
     var year = time.getFullYear()
     var box = document.getElementById('box').innerHTML = `${year}`;
-    setInterval(t2,1000);
+
 
 });
 
@@ -23,7 +23,7 @@ b3.addEventListener('click', function t3() {
     var time = new Date()
     var month = time.getMonth()
     var box = document.getElementById('box').innerHTML = `${month}`;
-    setInterval(t3,1000);
+   
 });
 
 
@@ -35,7 +35,7 @@ b4.addEventListener('click', function t4() {
     var min = time.getMinutes()
     var sec = time.getSeconds()
     var box = document.getElementById('box').innerHTML = `${full}:${min}:${sec}`;
-    setInterval(t4,1000);
+
 });
 
 //ddmmyyy format
@@ -43,7 +43,7 @@ var b5 = document.getElementById('b5')
 b5.addEventListener('click', function t5() {
     var time = new Date().toLocaleDateString()
     var box = document.getElementById('box').innerHTML = `${time}`;
-    setInterval(t5,1000);
+
 });
 
 //only date
@@ -52,6 +52,6 @@ b6.addEventListener('click', function t6() {
     var time = new Date()
     var day = time.getDate()
     var box = document.getElementById('box').innerHTML = `${day}`;
-  var timeout= setInterval(t6,1000);
+
 
 });
